e7ec23d3ce2a1b5261c0d454e0dde5e3e811af4d
park-do <joonkuy92@naver.com>
2018-11-30 15:38:55 +0900

    SQL 수정중

--- JAVA/src/MainClass.java
+++ JAVA/src/MainClass.java
@@ -2,5 +2,11 @@ public class MainClass {
     public static void main(String[] args)
     {
         System.out.println("HELLO WORLD");
+        DBSQLHandler.GetInstance();
+        System.out.println(DBSQLHandler.GetInstance().TryLogin("이가네","2234"));
+        DBSQLHandler.GetInstance().SearchRestaurant("","한식","rate");
+        //DBSQLHandler.GetInstance().WriteReview(3,"haha",100,2);
+        DBSQLHandler.GetInstance().GetReviews(100);
+        DBSQLHandler.GetInstance().GetRestaurantInformation(100);
     }
 }
